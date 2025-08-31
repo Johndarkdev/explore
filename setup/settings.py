b1fcb6f3c7f55ce607f49add9708e3f22be9c346
Johndarkdev <johndarkness04@gmail.com>
2025-08-31 18:21:43 +0100

    Actualizações finais

--- setup/settings.py
+++ setup/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config("SECRET_KEY", default="insecure-secret")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="explorexyz.onrender.com").split(",")
 
 # Apps
 THIRDY_PART_APPS = [
